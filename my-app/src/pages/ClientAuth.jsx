f12233a2a1ab6b62a1da0144bad6fa6cd683d712
Rajat <149293659+Rajat-0707@users.noreply.github.com>
2025-12-15 12:55:51 +0530

    Update ClientAuth.jsx

--- my-app/src/pages/ClientAuth.jsx
+++ my-app/src/pages/ClientAuth.jsx
@@ -18,7 +18,7 @@ const ClientAuth = () => {
       return;
     }
     try {
-      await fetchJSON(`${apiBase()}/register`, {
+      await fetchJSON(`${apiBase()}/api/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
